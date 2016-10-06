3092236bea513ed466b4c5021e5c5ffb98522e7f
Lotzi Boloni <lotzi.boloni@gmail.com>
2016-10-06 13:33:49 -0700

    Editing the batch file.

--- src/scripts/xapagy.bat
+++ src/scripts/xapagy.bat
@@ -1,7 +1,8 @@
 @rem echo %0
 @set CURRENTDIR=%~dp0
+@set JAVAPATH="c:\program files\java\jre-9\bin"
 @set JAVA=java
 @set XAPAGYCLASSPATH=%CURRENTDIR%/bin
 @set XAPAGYLIBPATH=%CURRENTDIR%/../XapagyLib
 @set XAPAGYLIBS=%XAPAGYLIBPATH%\jfreechart-1.0.15;%XAPAGYLIBPATH%\jcommon-1.0.18
-@%JAVA%  -classpath %XAPAGYCLASSPATH%;%XAPAGYLIBS% com.xapagy.Xapagy %* 
+@%JAVAPATH%\%JAVA%  -classpath %XAPAGYCLASSPATH%;%XAPAGYLIBS% com.xapagy.Xapagy %* 
