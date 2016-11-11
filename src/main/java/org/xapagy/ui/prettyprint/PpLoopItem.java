6ce74409b11ec00da502085736491673a0d331b6
Lotzi Boloni <lotzi.boloni@gmail.com>
2016-11-11 10:58:56 -0800

    Headers

--- src/main/java/org/xapagy/ui/prettyprint/PpLoopItem.java
+++ src/main/java/org/xapagy/ui/prettyprint/PpLoopItem.java
@@ -1,12 +1,22 @@
 /*
-   This file is part of the Xapagy project
-   Created on: Sep 14, 2011
- 
-   org.xapagy.ui.prettyprint.PpLoopItem
- 
-   Copyright (c) 2008-2014 Ladislau Boloni
- */
+   
+    This file is part of the Xapagy Cognitive Architecture 
+    Copyright (C) 2008-2017 Ladislau Boloni
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   
+*/
 package org.xapagy.ui.prettyprint;
 
 import org.xapagy.agents.Agent;
@@ -27,7 +37,7 @@ import org.xapagy.agents.AbstractLoopItem;
  * Pretty printing the loop item
  * 
  * @author Ladislau Boloni
- * 
+ * Created on: Sep 14, 2011
  */
 public class PpLoopItem {
 
--- src/main/java/org/xapagy/ui/prettyprint/PpSuccession.java
+++ src/main/java/org/xapagy/ui/prettyprint/PpSuccession.java
@@ -1,12 +1,22 @@
 /*
-   This file is part of the Xapagy project
-   Created on: Jan 2, 2011
- 
-   org.xapagy.ui.format.tostring.tostringPreSucc
- 
-   Copyright (c) 2008-2010 Ladislau Boloni
- */
+   
+    This file is part of the Xapagy Cognitive Architecture 
+    Copyright (C) 2008-2017 Ladislau Boloni
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   
+*/
 package org.xapagy.ui.prettyprint;
 
 import java.util.HashMap;
@@ -26,7 +36,7 @@ import org.xapagy.set.ViSet;
  * with the same prefix
  * 
  * @author Ladislau Boloni
- * 
+ * Created on: Jan 2, 2011
  */
 public class PpSuccession {
     /**
